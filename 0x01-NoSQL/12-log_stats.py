91e20e2644c2aea043dde6cfacc27f546675158e
Adejare <adejare.arasheed@gmail.com>
2024-06-22 03:05:32 +0100

    NoSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,49 +4,25 @@
 from pymongo import MongoClient
 
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+def log_stats(log_collection):
+    """provides some stats about Nginx logs stored in MongoDB"""
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
+    print(f'{log_collection.count_documents({})} logs')
+    print('Methods:')
+    for method in methods:
+        count = log_collection.count_documents({"method": f'{method}'})
+        print(f'\tmethod {method}: {count}')
+    method_path = log_collection.count_documents(
+        {
+            "$and": [
+                {"method": 'GET'},
+                {"path": "/status"}
+            ]
+        }
+    )
 
-def log_stats(mongo_collection, option=None):
-    """
-    Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
-    print("Methods:")
-    for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
-
-
-# def log_stats(log_collection):
-#     """provides some stats about Nginx logs stored in MongoDB"""
-#     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-
-#     print(f'{log_collection.count_documents({})} logs')
-#     print('Methods:')
-#     for method in methods:
-#         count = log_collection.count_documents({"method": f'{method}'})
-#         print(f'\tmethod {method}: {count}')
-#     method_path = log_collection.count_documents(
-#         {
-#             "$and": [
-#                 {"method": 'GET'},
-#                 {"path": "/status"}
-#             ]
-#         }
-#     )
-
-#     print(f'{method_path} status check')
+    print(f'{method_path} status check')
 
 
 if __name__ == '__main__':
