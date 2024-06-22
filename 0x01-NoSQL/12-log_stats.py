2a58feb0cdf4020c145c0c75856a871656529893
Adejare <adejare.arasheed@gmail.com>
2024-06-22 02:45:02 +0100

    NoSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,23 +3,29 @@
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017')
-log_collection = client.logs.nginx
-print(f'{log_collection.count_documents({})} logs')
 
-methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-print('Methods:')
-for method in methods:
-    count = log_collection.count_documents({"method": f'{method}'})
-    print(f'\tmethod {method}: {count}')
-method_path = log_collection.count_documents(
-    {
-        "$and": [
-            {"method": 'GET'},
-            {"path": "/status"}
-        ]
-    }
-)
+def log_stats():
+    """provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('mongodb://localhost:27017')
+    log_collection = client.logs.nginx
+    print(f'{log_collection.count_documents({})} logs')
 
-print(f'{method_path} status check')
-# if __name__ == '__main__':
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print('Methods:')
+    for method in methods:
+        count = log_collection.count_documents({"method": f'{method}'})
+        print(f'\tmethod {method}: {count}')
+    method_path = log_collection.count_documents(
+        {
+            "$and": [
+                {"method": 'GET'},
+                {"path": "/status"}
+            ]
+        }
+    )
+
+    print(f'{method_path} status check')
+
+
+if __name__ == '__main__':
+    log_stats()
